fab0951cd055ac90003e07d61e765ef6185556bf
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2025-02-02 21:55:42 +0100

    v0.0 | Comienzo del desarrollo de Fortune-api, desarrollada para gestionar las peticiones entre la base de datos y la aplicacion movil

---
+++ src/main/java/com/fortune_api/FortuneApiApplication.java
@@ -0,0 +1,13 @@
+package com.fortune_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FortuneApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FortuneApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fortune_api/FortuneApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fortune_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FortuneApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
