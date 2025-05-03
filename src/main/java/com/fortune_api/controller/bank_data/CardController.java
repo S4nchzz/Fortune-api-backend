492934340a661090e19a69be95f717a0fae9805a
Iyan Sanchez <iyannsanchezzdc4@gmail.com>
2025-05-03 04:06:55 +0200

    Nuevo metodo para gestionar el bloqueo de tarjetas

--- src/main/java/com/fortune_api/controller/bank_data/CardController.java
+++ src/main/java/com/fortune_api/controller/bank_data/CardController.java
@@ -3,16 +3,17 @@ package com.fortune_api.controller.bank_data;
 import com.fortune_api.db.entities.UserEntity;
 import com.fortune_api.db.entities.bank_data.AccountEntity;
 import com.fortune_api.db.entities.bank_data.CardEntity;
+import com.fortune_api.db.entities.bank_data.MovementCardEntity;
 import com.fortune_api.db.services.bank_data.AccountService;
+import com.fortune_api.db.services.bank_data.CardService;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -20,6 +21,9 @@ import java.util.List;
 public class CardController {
     @Autowired
     private AccountService accountService;
+
+    @Autowired
+    private CardService cardService;
 
     @GetMapping("/findMainCard")
     public ResponseEntity<?> getMainCard() {
@@ -40,5 +44,47 @@ public class CardController {
 
         AccountEntity account = accountService.findAccountByProprietary(user.getId());
         return ResponseEntity.ok(account.getCards().stream().toList());
+    }
+
+    @PostMapping("/manageCardLock")
+    public ResponseEntity<?> findMovements(@RequestBody String cardUUID) {
+        JSONObject json = new JSONObject(cardUUID);
+
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+
+        AccountEntity userAccount = accountService.findAccountByProprietary(user.getId());
+
+        boolean cardLockState = false;
+
+        for(CardEntity c : userAccount.getCards()) {
+            if (c.getCard_uuid().equals(json.get("card_uuid"))) {
+                c.setBlocked(!c.isBlocked());
+                cardLockState = c.isBlocked();
+
+                cardService.saveCard(c);
+            }
+        }
+
+        return ResponseEntity.ok(new JSONObject()
+                .put("locked", cardLockState)
+                .toString()
+        );
+    }
+
+
+    private CardEntity getCard(String cardUUID) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        UserEntity user = (UserEntity) authentication.getPrincipal();
+
+        AccountEntity userAccount = accountService.findAccountByProprietary(user.getId());
+
+        for(CardEntity c : userAccount.getCards()) {
+            if (c.getCard_uuid().equals(cardUUID)) {
+                return c;
+            }
+        }
+
+        return null;
     }
 }
